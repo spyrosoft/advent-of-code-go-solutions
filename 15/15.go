a30540ba35139ee4af7bc2dbd8ca55844728d1fb
Bennett Roesch <meta@spyrosoft.com>
2015-12-25 02:26:39 -0800

    Rework 15 and add notes - not working.

--- 15/15.go
+++ 15/15.go
@@ -10,18 +10,6 @@ import (
 	"regexp"
 )
 
-var (
-	permutationsArray [][]int
-	ingredientsList = list.New()
-	ingredientsArray [][]int
-	nonAlphaNumericRegex = regexp.MustCompile( "[^a-zA-Z0-9- ]" )
-	optimalPermutationValue = 0
-	capacityIndex = 0
-	durabilityIndex = 1
-	flavorIndex = 2
-	textureIndex = 3
-)
-
 func exitOnError( error error ) {
 	if error != nil {
 		fmt.Println( error )
@@ -29,68 +17,116 @@ func exitOnError( error error ) {
 	}
 }
 
-func convertIngredientsListToArray() {
-	ingredientsArray = make( [][]int, ingredientsList.Len() )
-	i := 0
-	for element := ingredientsList.Front(); element != nil; element = element.Next() {
-		ingredientsArray[ i ] = element.Value.([]int)
-		i++
-	}
+func stringToInteger( string string ) int {
+	integer, error := strconv.Atoi( string )
+	exitOnError( error )
+	return integer
 }
 
-func buildIngredients() {
+func buildList( integers ...int ) *list.List {
+	newList := list.New()
+	for _, integer := range integers {
+		newList.PushBack( integer )
+	}
+	return newList
+}
+
+func listNth( list *list.List, nth int ) int {
+	element := list.Front()
+	for i := 0; i < nth; i++ {
+		element = element.Next()
+	}
+	return element.Value.(int)
+}
+
+func convert2DListTo2DArray( twoDimensionalList *list.List ) [][]int {
+	convertedArray := make( [][]int, twoDimensionalList.Len() )
+	i := 0
+	for element := twoDimensionalList.Front(); element != nil; element = element.Next() {
+		secondDimension := make( []int, element.Value.(*list.List).Len() )
+		secondDimensionIndex := 0
+		for secondDimensionElement := element.Value.(*list.List).Front(); secondDimensionElement != nil; secondDimensionElement = secondDimensionElement.Next() {
+			secondDimension[ secondDimensionIndex ] = secondDimensionElement.Value.(int)
+			secondDimensionIndex++
+		}
+		convertedArray[ i ] = secondDimension
+		i++
+	}
+	return convertedArray
+}
+
+func buildIngredients() *list.List {
 	file, error := os.Open( "input.txt" )
 	exitOnError( error )
 	defer file.Close()
 	scanner := bufio.NewScanner( file )
 	
+	nonAlphaNumericRegex := regexp.MustCompile( "[^a-zA-Z0-9- ]" )
+	ingredients := list.New()
+	
 	for scanner.Scan() {
 		alphaNumericInput := nonAlphaNumericRegex.ReplaceAllString( scanner.Text(), "" )
 		input := strings.Split( alphaNumericInput, " " )
 		
-		capacity, error := strconv.Atoi( input[ 2 ] )
-		exitOnError( error )
-		durability, error := strconv.Atoi( input[ 4 ] )
-		exitOnError( error )
-		flavor, error := strconv.Atoi( input[ 6 ] )
-		exitOnError( error )
-		texture, error := strconv.Atoi( input[ 8 ] )
-		exitOnError( error )
-		ingredientsList.PushBack( []int{ capacity, durability, flavor, texture } )
+		capacity := stringToInteger( input[ 2 ] )
+		durability := stringToInteger( input[ 4 ] )
+		flavor := stringToInteger( input[ 6 ] )
+		texture := stringToInteger( input[ 8 ] )
+		
+		ingredients.PushBack( buildList( capacity, durability, flavor, texture ) )
 	}
 	
-	convertIngredientsListToArray()
+	return ingredients
 }
 
-func addPermutation( permutations *list.List ) {
+func addPermutation( permutations *list.List, previousPermutation *list.List, index int, carry int ) {
+	permutation := list.New()
+	for previousPermutationIndex := 0; previousPermutationIndex < index; previousPermutationIndex++ {
+		permutation.PushBack( listNth( permutations, previousPermutationIndex ) )
+	}
 	
+	permutation.PushBack(  )
+	
+	if index < permutations.Front().Len() {
+		addPermutation( permutations, index + 1, carry )
+	}
 }
 
-func buildPermutations( numberOfPossibilities int, numberOfAttributes int ) {
+func buildPermutations( numberOfPossibilities int, numberOfAttributes int, ingredients [][]int ) *list.List {
 	permutations := list.New()
 	
 	firstPermutation := list.New()
-	for i := 0; i < ingredientsList.Len(); i++ {
+	for _, _ = range ingredients {
 		firstPermutation.PushBack( 1 )
 	}
+	
 	firstPermutation.Front().Value = numberOfPossibilities - ( numberOfAttributes - 1 )
 	
 	permutations.PushBack( firstPermutation )
-	addPermutation( permutations )
+	addPermutation( permutations, firstPermutation, 0, 0 )
+	
+	return permutations
 }
 
-func checkForOptimalIngredients( permutations []int ) {
+func checkForOptimalIngredients( permutations []int, ingredients [][]int ) int {
+	optimalPermutationValue := 0
+	
+	capacityIndex := 0
+	durabilityIndex := 1
+	flavorIndex := 2
+	textureIndex := 3
+	
 	capacityTotal := 0
 	durabilityTotal := 0
 	flavorTotal := 0
 	textureTotal := 0
 	
 	fmt.Println( permutations )
-	for i := 0; i < ingredientsList.Len(); i++ {
-		capacityTotal += permutations[ i ] * ingredientsArray[ i ][ capacityIndex ]
-		durabilityTotal += permutations[ i ] * ingredientsArray[ i ][ durabilityIndex ]
-		flavorTotal += permutations[ i ] * ingredientsArray[ i ][ flavorIndex ]
-		textureTotal += permutations[ i ] * ingredientsArray[ i ][ textureIndex ]
+	for i, ingredient := range ingredients {
+		capacityTotal += permutations[ i ] * ingredient[ capacityIndex ]
+		durabilityTotal += permutations[ i ] * ingredient[ durabilityIndex ]
+		flavorTotal += permutations[ i ] * ingredient[ flavorIndex ]
+		textureTotal += permutations[ i ] * ingredient[ textureIndex ]
 	}
 	
 	if capacityTotal < 0 { capacityTotal = 0 }
@@ -103,40 +139,20 @@ func checkForOptimalIngredients( permutations []int ) {
 	if permutationProduct > optimalPermutationValue {
 		optimalPermutationValue = permutationProduct
 	}
+	
+	return optimalPermutationValue
 }
 
-// func permuteCookies( numberOfPossibilities int, numberOfAttributes int ) {
-// 	max := numberOfPossibilities - ( numberOfAttributes - 1 )
-// 	min := 1
-// 	permutations := make( []int, numberOfAttributes )
-// 	for i := 0; i < numberOfAttributes; i++ {
-// 		permutations[ i ] = min
-// 	}
-// 	permutations[ 0 ] = max
-// 	carry := 0
-// 	index := numberOfAttributes - 1
-// //	checkForOptimalIngredients( permutations )
-// 	for {
-// 		if permutations[ index ] > min {
-// 			carry++
-// 			permutations[ index ]--
-// 			permutations[ index + 1 ] += carry
-// 			index = numberOfAttributes - 1
-// 			carry = 0
-// 			fmt.Println(permutations)
-// //			checkForOptimalIngredients( permutations )
-// 		}
-// 		if index == 0 && permutations[ index ] == min {
-// 			break
-// 		}
-// 		index--
-// 	}
-// }
+func permuteCookies( permutations [][]int, ingredients [][]int ) {
+	
+}
 
 func main() {
-	buildIngredients()
-	buildPermutations( 8, 4 )
-	//permuteCookies( 100, ingredientsList.Len() )
-	//permuteCookies( 8, 4 )
-	fmt.Println( optimalPermutationValue )
+	ingredientsList := buildIngredients()
+	ingredients := convert2DListTo2DArray( ingredientsList )
+	permutationsList := buildPermutations( 7, len( ingredients ), ingredients )
+	permutations := convert2DListTo2DArray( permutationsList )
+	fmt.Println( permutations )
+	permuteCookies( permutations, ingredients )
+	//fmt.Println( optimalPermutationValue )
 }
